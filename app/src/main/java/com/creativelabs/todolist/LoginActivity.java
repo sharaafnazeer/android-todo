1d4c1d7532f73dc1a3f22a2ce0098dad690cd163
Sharaaf Nazeer <Sharaaf.Nazeer@author-it.com>
2021-05-25 19:57:54 +0530

    Initial and Login page

---
+++ app/src/main/java/com/creativelabs/todolist/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.creativelabs.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView tvNewUser;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        tvNewUser = findViewById(R.id.tvRegisterView);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        tvNewUser.setOnClickListener(v -> {
+            Intent registerIntent = new Intent(this, RegisterActivity.class);
+            startActivity(registerIntent);
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            startActivity(mainIntent);
+            finish(); // To finish current activity
+        });
+    }
+}
